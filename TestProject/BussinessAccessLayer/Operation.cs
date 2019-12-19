5c9cd86baa9ea31639317b1f76a1c150f96c94ca
prasavtalent <prasad1vtalent@gmail.com>
2019-12-19 17:43:11 +0530

    operation changes method

---
+++ TestProject/BussinessAccessLayer/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BussinessAccessLayer
+{
+    public class Operation
+    {
+        public int Addition()
+        {
+            int a = 10;
+            int b =20;
+            int c= a + b;
+            return c;
+        }
+    }
+}
